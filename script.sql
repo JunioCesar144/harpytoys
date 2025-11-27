56d1a18d7be071d0b246aa9db633ad3ca4f5c2a3
junio <juniocesar19636@gmail.com>
2025-11-27 19:33:43 -0300

    subindo o projeto tcc para o github

---
+++ script.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE bdEcommerce;
+
+USE bdEcommerce;
+
+CREATE TABLE Usuario(
+	Id int primary key auto_increment,
+    Nome varchar(50) not null,
+    Email varchar(50) not null,
+    Senha varchar(50) not null
+);
+
+CREATE TABLE Cliente(
+	CodCli int primary key auto_increment,
+    NomeCli varchar(50) not null,
+    TelCli varchar(20) not null,
+    EmailCli varchar(50) not null
+)
+
+SELECT * FROM Usuario;
+SELECT * FROM Cliente;
+
+
+admin@emailcom
+
+12345678
+
+Teste@gmail.com
+123456789
